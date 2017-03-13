c8338f936cbfd10693a2bf9d757578701cf54cf2
nlee14 <n.lee14@student.unimelb.edu.au>
2017-03-13 17:12:12 +1100

    Update Submission.java

--- Submission.java
+++ Submission.java
@@ -7,9 +7,6 @@ class Submission {
 	private File[] files;
     // Return two fake files
     public File[] includedFiles(){
-        File[] files = new File[2];
-        files[0] = new File();
-        files[1] = new File();
         return files;
     }
 }
