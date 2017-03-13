0903e839bdd5d4205390f02b2db64d8c23cc6d51
nozomioishi <Nozomi_Oishi@hotmail.com>
2017-03-13 17:07:31 +1100

    Add files via upload

--- Subject.java
+++ Subject.java
@@ -1,9 +1,12 @@
 package com.unimelb.swen30006.workshops;
 
+import java.util.ArrayList;
+
 public class Subject {
 	
 	public String name;
 	public String subjectCode;
+	public ArrayList<File[]> assignments = new ArrayList<File[]>();
 	
 	public Subject(String subjectName, String code)
 	{
@@ -11,9 +14,9 @@ public class Subject {
 		subjectCode = code;
 	}
 	
-	public void createAssignment()
+	public void createAssignment(File[] file)
 	{
-	
+		assignments.add(file);
 	}
 	
 	public void deleteAssignment(String name)
